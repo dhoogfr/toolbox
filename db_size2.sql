51379bf004838288067b6fa1e05686b689917118
dhoogfr <freek.dhooge@gmail.com>
2013-06-07 13:39:58 +0200

    added the posiblity to filter on the name of the tablespace

--- db_size2.sql
+++ db_size2.sql
@@ -1,12 +1,18 @@
 set pagesize 999
-set linesize 140
+set linesize 200
+set verify off
 
-column name format a25 heading "tablespace name" 
-column space_mb format 99g999g990D99 heading "curr df mbytes" 
-column maxspace_mb format 99g999g990D99 heading "max df mbytes" 
-column used format 99g999g990D99 heading "used mbytes" 
-column df_free format 99g999g990D99 heading "curr df free mbytes"
-column maxdf_free format 99g999g990D99 heading "max df free mbytes"
+set feedback off
+column inputpar01 new_value 1 noprint
+select 1 inputpar01 from dual where 1=2;
+set feedback 6
+
+column name format a25 heading "tablespace name"
+column space_mb format 9G999g999g990D99 heading "curr df mbytes"
+column maxspace_mb format 9G999g999g990D99 heading "max df mbytes"
+column used format 9G999g999g990D99 heading "used mbytes"
+column df_free format 9G999g999g990D99 heading "curr df free mbytes"
+column maxdf_free format 9G999g999g990D99 heading "max df free mbytes"
 column pct_free format 990D99 heading "% free"
 column pct_maxfile_free format 990D99 heading "% maxfile free"
 
@@ -18,10 +24,10 @@ compute sum of df_free on report
 compute sum of maxdf_free on report
 compute sum of used on report
 
- 
+
 select df.tablespace_name name, df.space space_mb, df.maxspace maxspace_mb, (df.space - nvl(fs.freespace,0)) used,
-       nvl(fs.freespace,0) df_free, (nvl(fs.freespace,0) + df.maxspace - df.space) maxdf_free, 
-       100 * (nvl(fs.freespace,0) / df.space) pct_free, 
+       nvl(fs.freespace,0) df_free, (nvl(fs.freespace,0) + df.maxspace - df.space) maxdf_free,
+       100 * (nvl(fs.freespace,0) / df.space) pct_free,
        100 * ((nvl(fs.freespace,0) + df.maxspace - df.space) / df.maxspace) pct_maxfile_free
 from ( select tablespace_name, sum(bytes)/1024/1024 space, sum(greatest(maxbytes,bytes))/1024/1024 maxspace
        from dba_data_files
@@ -36,4 +42,7 @@ from ( select tablespace_name, sum(bytes)/1024/1024 space, sum(greatest(maxbytes
        group by tablespace_name
      ) fs
 where df.tablespace_name = fs.tablespace_name(+)
+      and df.tablespace_name like nvl('&1', '%')
 order by name;
+
+undef 1
