dad6d1f996ff7aedb84c6a6be219cf4e691c59fd
dhoogfr <freek.dhooge@gmail.com>
2013-09-25 10:01:26 +0200

    added the list_incoming_db_links.sql script
    list_incoming_db_links.sql
      uses the aud$ data to generate a list of all incoming database links
      the accurateness of the list depends on how long the audit data is stored and inactive db links will not show up in this list

---
+++ list_incoming_db_links.sql
@@ -0,0 +1,44 @@
+-- uses the aud$ data to generate a list of all incoming database links
+-- the accurateness of the list depends on how long the audit data is stored and inactive db links will not show up in this list
+
+set linesize 250
+
+column globalname format a30
+column userhost format a40
+column userid format a30
+column first_connection format a20
+column last_connection format a20
+
+break on globalname on userhost
+
+with
+  incoming as
+    ( select 
+        regexp_replace(comment$text, '.*=(.*)\..*', '\1') globalname,
+        userhost,
+        userid,
+        ntimestamp#
+      from 
+        sys.aud$ 
+      where 
+        comment$text like 'DBLINK%' 
+    )
+select
+  globalname,
+  userhost,
+  userid,
+  to_char(min(ntimestamp#),'DD/MM/YYYY HH24:MI') first_connection,
+  to_char(max(ntimestamp#), 'DD/MM/YYYY HH24:MI') last_connection
+from
+  incoming  
+group by
+  globalname,
+  userhost,
+  userid
+order by
+  globalname,
+  userhost,
+  userid
+;re
+
+clear breaks
