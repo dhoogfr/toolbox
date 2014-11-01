acae60b0dcd44240abcd65b447ebdc95cb9d257f
dhoogfr <freek.dhooge@gmail.com>
2014-11-01 17:56:07 +0100

    added last_call_et to the selection list

--- list_sessions.sql
+++ list_sessions.sql
@@ -1,4 +1,4 @@
-set linesize 130
+set linesize 150
 set pagesize 999
 column sid format 9999
 column machine format a20
@@ -6,6 +6,6 @@ column osuser format a20
 column user format a15
 column program format a30
 
-select sid, serial#, username, machine, osuser, program, server
+select sid, serial#, username, machine, osuser, program, server, last_call_et
 from v$session
 where username is not null;
