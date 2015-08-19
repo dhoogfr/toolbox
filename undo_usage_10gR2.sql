26044bb4895c59a04206fa5c2dd7c33911fb9684
dhoogfr <freek.dhooge@gmail.com>
2015-08-19 17:54:35 +0200

    fixed a bug with incorrect calculation of the query and tuned query time and made the query rac aware
    In the query, the max query length and the tuned undo retention are converted to a "days hours:minutes:seconds" format using the extract function.
    This means that the value must be of the timestamp datatype and not integer.
    To convert the original integer value to a timestamp, the following trick was used: systimestamp + integer/60/60/24 - systimestamp
    However, because of implicit data type conversion when adding an integer to a timestamp value, the timezone information on the first systimestamp was lost (as the resulting datatype is date, not timestamp). This gives a wrong result when the second systimestamp value is subtracted from the result.
    
    To fix this, the integer was converted to an interval first: systimestamp + numtodsinterval(integer, 'seconds') - systimestamp
    
    The query was also made rac aware by switching to gv$undostat and adding the inst_id column to the relevant places
    
    query formatting was changed for better readability

--- undo_usage_10gR2.sql
+++ undo_usage_10gR2.sql
@@ -11,9 +11,16 @@ accept edate prompt 'end date: '
 
 column undo_retention_time format a20
 
-select value undo_retention_time
-from v$parameter
-where name = 'undo_retention';
+select
+  inst_id, 
+  value undo_retention_time
+from
+  gv$parameter
+where
+  name = 'undo_retention'
+order by
+  inst_id
+;
 
 
 column undo_space format 999G999G999D99
@@ -22,13 +29,19 @@ column undo_tbs format a15
 column q_length format a12
 column tu_length format a12
 
-select sum(bytes)/1024/1024 undo_space, sum(maxbytes)/1024/1024 max_undo_space
-from dba_data_files
-where tablespace_name = 
-        ( select value 
-          from v$parameter 
-          where name = 'undo_tablespace'
-        );
+select
+  tablespace_name, 
+  sum(bytes)/1024/1024 undo_space, 
+  sum(maxbytes)/1024/1024 max_undo_space
+from
+  dba_data_files
+where 
+  tablespace_name in (select value from gv$parameter where name = 'undo_tablespace')
+group by 
+  tablespace_name
+order by
+  tablespace_name
+;
 
 column begin_time format a17
 column end_time format a17
@@ -38,48 +51,57 @@ column pct_used format 999D99
 column ts_ss format 99D99
 column err format a12
 column maxqueryid format a15
+column inst_id format 999 
 
 prompt err: unxpblkreucnt / ssolderrcnt / nospaceerrcnt
 
-select to_char(begin_time,'DD/MM/YYYY HH24:MI') begin_time, 
-       to_char(end_time,'DD/MM/YYYY HH24:MI') end_time,
-       (select name from v$tablespace where ts# = undotsn) undo_tbs, 
-       curr_undo_usage, retention_undo_usage,
-       (100 * retention_undo_usage /  
-        ( select sum(greatest(bytes,maxbytes))/1024/1024
-          from dba_data_files
-          where tablespace_name = (select name from v$tablespace where ts# = undotsn)
-        )
-       ) pct_used,
-       ts_dd || ' ' || ts_hh || ':' || ts_mi || ':' || ts_ss q_length,
-       tund_dd || ' ' || tund_hh || ':' || tund_mi || ':' || tund_ss tu_length,
-       maxqueryid, err
-from ( select undotsn,
-              begin_time, end_time, 
-              undoblks * 
-                ( select block_size 
-                  from dba_tablespaces 
-                  where tablespace_name = (select name from v$tablespace where ts# = undotsn)
-                ) /1024/1024 curr_undo_usage,
-                ( (activeblks + unexpiredblks)
-                  * ( select block_size 
-                      from dba_tablespaces 
-                      where tablespace_name = (select name from v$tablespace where ts# = undotsn)
-                    ) 
-                  /1024/1024
-                ) retention_undo_usage,
-                  decode(maxquerylen, 0, 0, extract(day from (systimestamp + maxquerylen/24/60/60 - systimestamp))) ts_dd,
-                  decode(maxquerylen, 0, 0, extract(hour from (systimestamp + maxquerylen/24/60/60 - systimestamp))) ts_hh,
-                  decode(maxquerylen, 0, 0, extract(minute from (systimestamp + maxquerylen/24/60/60 - systimestamp))) ts_mi,
-                  decode(maxquerylen, 0, 0, floor(extract(second from (systimestamp + maxquerylen/24/60/60 - systimestamp)))) ts_ss,
-                  decode(tuned_undoretention, 0, 0, extract(day from (systimestamp + tuned_undoretention/24/60/60 - systimestamp))) tund_dd,
-                  decode(tuned_undoretention, 0, 0, extract(hour from (systimestamp + tuned_undoretention/24/60/60 - systimestamp))) tund_hh,
-                  decode(tuned_undoretention, 0, 0, extract(minute from (systimestamp + tuned_undoretention/24/60/60 - systimestamp))) tund_mi,
-                  decode(tuned_undoretention, 0, 0, floor(extract(second from (systimestamp + tuned_undoretention/24/60/60 - systimestamp)))) tund_ss,
-                  maxqueryid, unxpblkreucnt || '/' || ssolderrcnt  || '/' || nospaceerrcnt err
-       from v$undostat 
-) U
-where begin_time >= to_date('&bdate', 'DD/MM/YYYY HH24:MI')
-      and end_time <= to_date('&edate', 'DD/MM/YYYY HH24:MI')
-order by U.begin_time;
+break on inst_id skip 1
 
+select
+  inst_id, 
+  to_char(begin_time,'DD/MM/YYYY HH24:MI') begin_time, 
+  to_char(end_time,'DD/MM/YYYY HH24:MI') end_time,
+  ( select
+      name
+    from
+      v$tablespace
+    where
+      ts# = undotsn
+  ) undo_tbs, 
+  curr_undo_usage, 
+  retention_undo_usage,
+  (100 * retention_undo_usage / (select sum(greatest(bytes,maxbytes))/1024/1024 from dba_data_files where tablespace_name = (select name from v$tablespace where ts# = undotsn))) pct_used,
+  ts_dd || ' ' || ts_hh || ':' || ts_mi || ':' || ts_ss q_length,
+  tund_dd || ' ' || tund_hh || ':' || tund_mi || ':' || tund_ss tu_length,
+  maxqueryid,
+  err
+from 
+  ( select
+      inst_id,
+      undotsn,
+      begin_time,
+      end_time, 
+      undoblks * (select block_size from dba_tablespaces where tablespace_name = (select name from v$tablespace where ts# = undotsn)) /1024/1024 curr_undo_usage,
+      ((activeblks + unexpiredblks) * (select block_size from dba_tablespaces where tablespace_name = (select name from v$tablespace where ts# = undotsn)) /1024/1024) retention_undo_usage,
+      decode(maxquerylen, 0, 0, extract(day from (systimestamp + numtodsinterval(maxquerylen,'second') - systimestamp))) ts_dd,
+      decode(maxquerylen, 0, 0, extract(hour from (systimestamp + numtodsinterval(maxquerylen,'second') - systimestamp))) ts_hh,
+      decode(maxquerylen, 0, 0, extract(minute from (systimestamp + numtodsinterval(maxquerylen,'second') - systimestamp))) ts_mi,
+      decode(maxquerylen, 0, 0, floor(extract(second from (systimestamp + numtodsinterval(maxquerylen,'second') - systimestamp)))) ts_ss,
+      decode(tuned_undoretention, 0, 0, extract(day from (systimestamp + numtodsinterval(tuned_undoretention,'second') - systimestamp))) tund_dd,
+      decode(tuned_undoretention, 0, 0, extract(hour from (systimestamp + numtodsinterval(tuned_undoretention,'second') - systimestamp))) tund_hh,
+      decode(tuned_undoretention, 0, 0, extract(minute from (systimestamp + numtodsinterval(tuned_undoretention,'second') - systimestamp))) tund_mi,
+      decode(tuned_undoretention, 0, 0, floor(extract(second from (systimestamp + numtodsinterval(tuned_undoretention,'second') - systimestamp)))) tund_ss,
+      maxqueryid, 
+      unxpblkreucnt || '/' || ssolderrcnt  || '/' || nospaceerrcnt err
+    from 
+      gv$undostat 
+  ) U
+where
+  begin_time >= to_date('&bdate', 'DD/MM/YYYY HH24:MI')
+  and end_time <= to_date('&edate', 'DD/MM/YYYY HH24:MI')
+order by
+  inst_id, 
+  U.begin_time
+;
+
+clear breaks
