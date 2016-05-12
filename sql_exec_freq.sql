f653b433438d1256941637cd2e80515522ef3ca2
dhoogfr <freek.dhooge@gmail.com>
2016-05-12 23:13:42 +0200

    Fix issue when executions_delta is 0 in sql_exec_freq.sql script
    Fix an issue causing the script to fail when the executions_delta was 0,
    which could happen if the statement did never finish within the sample period

--- sql_exec_freq.sql
+++ sql_exec_freq.sql
@@ -7,9 +7,9 @@ column buffer_gets format 999999999
 select to_char(snap.begin_interval_time, 'DD/MM/YYYY HH24:MI:SS') btime, 
        to_char(snap.end_interval_time, 'DD/MM/YYYY HH24:MI:SS') etime, snap.snap_id, 
        snap.instance_number, sqlstat.executions_delta nbr_executions, 
-       (sqlstat.elapsed_time_delta / sqlstat.executions_delta/1000000) avg_elapsed_sec,
-       (sqlstat.disk_reads_delta / sqlstat.executions_delta) disk_reads, 
-       (sqlstat.buffer_gets_delta / sqlstat.executions_delta) buffer_gets
+       (sqlstat.elapsed_time_delta / decode(sqlstat.executions_delta, 0, 1, executions_delta)/1000000) avg_elapsed_sec,
+       (sqlstat.disk_reads_delta / decode(sqlstat.executions_delta, 0, 1, executions_delta)) disk_reads, 
+       (sqlstat.buffer_gets_delta / decode(sqlstat.executions_delta, 0, 1, executions_delta)) buffer_gets
 from dba_hist_snapshot snap, dba_hist_sqlstat sqlstat
 where snap.snap_id = sqlstat.snap_id(+)
       and snap.instance_number = sqlstat.instance_number(+)
