33a81f7c0de5902528f0da25a4964c3e116f50d1
dhoogfr <freek.dhooge@gmail.com>
2016-05-12 17:59:55 +0200

    Make sessinfo2.sql rac aware
    Change the sessino2.sql script to use the gv$ views to make it rac aware

--- sessinfo2.sql
+++ sessinfo2.sql
@@ -1,5 +1,6 @@
 set linesize 150
 set verify off
+set long 50000
 
 column username format a20
 column program format a15
@@ -11,13 +12,16 @@ column sql_child_number new_value  v_sql_child_number
 
 select sess.sid, sess.serial#, prc.spid, sess.username, sess.program, sess.osuser, 
        sess.machine, sess.sql_id, sess.sql_child_number
-from v$session sess, v$process prc
-where sess.paddr = prc.addr
-      and sess.sid = '&sid';
+from gv$session sess, gv$process prc
+where sess.inst_id = prc.inst_id
+      and sess.paddr = prc.addr
+      and sess.sid = '&sid'
+      and sess.inst_id = &inst;
 
 select sql_fulltext
-from v$sqlarea
+from gv$sqlarea
 where sql_id = '&v_sql_id';      
 
 select * 
 from table(dbms_xplan.display_cursor('&v_sql_id', &v_sql_child_number));
+
