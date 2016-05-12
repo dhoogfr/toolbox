f6d102bef112cc5395192c342b5cc78ac2236c75
dhoogfr <freek.dhooge@gmail.com>
2016-05-12 23:00:24 +0200

    Correct numtodsinterval usage in the longops2.sql query
    Corrected a bug where the numtodsinterval function unit was using seconds instead of second

--- longops2.sql
+++ longops2.sql
@@ -11,18 +11,19 @@ column pct_complete format 09D00
 select
   sid, serial#, sql_id, to_char(start_time, 'DD/MM/YYYY HH24:MI:SS') start_time, opname,
   round((100 * sofar)/totalwork, 2)  pct_complete,
-  ( extract(day from (systimestamp + numtodsinterval(elapsed_seconds, 'seconds') - systimestamp)) || ' ' ||
-    extract(hour from (systimestamp + numtodsinterval(elapsed_seconds, 'seconds') - systimestamp)) || ':' ||
-    extract(minute from (systimestamp + numtodsinterval(elapsed_seconds, 'seconds') - systimestamp))  || ':' ||
-    round(extract(second from (systimestamp + numtodsinterval(elapsed_seconds, 'seconds') - systimestamp)))
+  ( extract(day from (systimestamp + numtodsinterval(elapsed_seconds, 'second') - systimestamp)) || ' ' ||
+    extract(hour from (systimestamp + numtodsinterval(elapsed_seconds, 'second') - systimestamp)) || ':' ||
+    extract(minute from (systimestamp + numtodsinterval(elapsed_seconds, 'second') - systimestamp))  || ':' ||
+    round(extract(second from (systimestamp + numtodsinterval(elapsed_seconds, 'second') - systimestamp)))  
   ) ET,
-  ( extract(day from (systimestamp + numtodsinterval(time_remaining, 'seconds') - systimestamp))  || ' ' ||
-    extract(hour from (systimestamp + numtodsinterval(time_remaining, 'seconds') - systimestamp))  || ':' ||
-    extract(minute from (systimestamp + numtodsinterval(time_remaining, 'seconds') - systimestamp))  || ':' ||
-    round(extract(second from (systimestamp + numtodsinterval(time_remaining, 'seconds') - systimestamp)))
+  ( extract(day from (systimestamp + numtodsinterval(time_remaining, 'second') - systimestamp))  || ' ' ||
+    extract(hour from (systimestamp + numtodsinterval(time_remaining, 'second') - systimestamp))  || ':' ||
+    extract(minute from (systimestamp + numtodsinterval(time_remaining, 'second') - systimestamp))  || ':' ||
+    round(extract(second from (systimestamp + numtodsinterval(time_remaining, 'second') - systimestamp))) 
   ) RT
 from
   v$session_longops
 where
-  time_remaining > 0;
+  time_remaining > 0
+;
 
