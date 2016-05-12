937583d87f16b74d48079fbda13ebf1ac9a5bed6
dhoogfr <freek.dhooge@gmail.com>
2016-05-12 09:09:32 +0200

    Add script to report on CBO statistics collection dates
    Add the cbo_last_analyzed_stats.sql to create a report on the age of
    CBO statistics in a given schema

---
+++ cbo_last_analyzed_stats.sql
@@ -0,0 +1,76 @@
+column counted format 9G999G999G999
+
+break on report on stattype_locked skip 2 on last_analyzed_day skip 1
+
+compute sum of counted on last_analyzed_day 
+compute sum of counted on stattype_locked 
+compute sum of counted on report
+
+select
+  stattype_locked,
+  trunc(last_analyzed) last_analyzed_day,
+  object_type,
+  count(*) counted
+from 
+  ( select
+      object_type, 
+      last_analyzed,
+      stattype_locked
+    from
+      dba_tab_statistics
+    where
+      owner = '&OWNER'
+      and table_name not in
+        ( select
+            table_name
+          from
+            dba_external_tables
+          where
+            owner = '&OWNER'
+          union all
+          select
+            table_name
+          from
+            dba_tables
+          where
+            temporary = 'Y'
+            and owner = '&OWNER'
+        )
+    union all
+    select
+      object_type, 
+      last_analyzed,
+      stattype_locked
+    from
+      dba_ind_statistics
+    where
+      owner = '&OWNER'
+      and table_name not in
+        ( select
+            table_name
+          from
+            dba_external_tables
+          where
+            owner = '&OWNER'
+          union all
+          select
+            table_name
+          from
+            dba_tables
+          where
+            temporary = 'Y'
+            and owner = '&OWNER'
+        )
+  )
+group by
+  stattype_locked,
+  trunc(last_analyzed),
+  object_type
+order by
+  stattype_locked,
+  last_analyzed_day,
+  object_type
+;
+
+clear breaks
+clear computes
