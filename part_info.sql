17621f1d3b65968860bf2060b6d748d853061e6f
dhoogfr <freek.dhooge@gmail.com>
2016-07-18 15:30:09 +0200

    Modify part_info.sql script for better output formatting
    Modify part_info.sql script to use column formatting and compute totals

--- part_info.sql
+++ part_info.sql
@@ -1,9 +1,27 @@
 -- show information about a partitoned table
--- TODO: format the output
 
+set verify off
 set linesize 250
 
+column owner format a30
+column table_name format a30
+column column_name format a30
 column interval format a40
+column high_value format a25
+column interval format a3
+column subpartition_count format 9999999 heading "#SUBPART"
+column partition_name format a30
+column subpartition_name format a30
+column column_position format 99999
+column interval format a3
+column subpartition_position format 99999
+
+accept OWNER prompt 'Owner: '
+accept TABLE prompt 'Table Name: '
+
+
+prompt
+prompt ###basic info
 
 select
   owner,
@@ -19,8 +37,9 @@ where
   and table_name = '&&TABLE'
 ;
 
-column_name format a30
-column column_position format 99999
+
+prompt
+prompt ###info on partition columns
 
 select
   column_position,
@@ -34,6 +53,10 @@ order by
   column_position
 ;
 
+
+prompt
+prompt ###info on sub-partition columns
+
 select
   column_position,
   column_name
@@ -45,11 +68,12 @@ where
 order by
   column_position
 ;
- 
 
-column high_value format a90
-column interval format a3
-column subpartition_count format 9999999 heading "#SubPart"
+
+prompt
+prompt ###details on partitions
+
+compute sum of mb on report
 
 select
   part.partition_name,
@@ -76,14 +100,16 @@ order by
   part.partition_position
 ;
 
+clear computes
 
 
-column high_value format a90
-column interval format a3
-column subpartition_position format 99999
+prompt
+prompt ###details on sub-partitions
 
-break on partition_name
-
+break on partition_name skip page duplicates
+compute sum of mb on partition_name
+compute sum of mb on report
+/*
 select
   spart.partition_name,
   spart.subpartition_name,
@@ -105,7 +131,7 @@ where
   and spart.partition_name = part.partition_name
   and spart.table_owner = seg.owner(+)
   and spart.table_name = seg.segment_name(+)
-  and spart.partition_name = seg.partition_name(+)
+  and spart.subpartition_name = seg.partition_name(+)
   and spart.table_owner = '&&OWNER'
   and spart.table_name = '&&TABLE'
 order by
@@ -113,7 +139,45 @@ order by
   spart.subpartition_position
 ;
 
+*/
+
+--switched to ansi join syntax (as excercise  ;-)  
+select
+  spart.partition_name,
+  spart.subpartition_name,
+  spart.high_value,
+  spart.subpartition_position,
+  spart.tablespace_name,
+  spart.compression,
+  spart.compress_for,
+  spart.interval,
+  spart.segment_created,
+  (seg.bytes/1024/1024) MB
+from
+  dba_tab_subpartitions                                       spart
+    join dba_tab_partitions                                   part
+      on ( part.table_owner = spart.table_owner
+           and part.table_name = spart.table_name
+           and part.partition_name = spart.partition_name
+         )
+      left outer join dba_segments                            seg
+        on ( seg.owner = spart.table_owner
+             and seg.segment_name = spart.table_name
+             and seg.partition_name = spart.subpartition_name
+           )
+where
+  spart.table_owner = '&&OWNER'
+  and spart.table_name = '&&TABLE'
+order by
+  part.partition_position,
+  spart.subpartition_position
+;
+
+
 clear breaks
+clear computes
+
 
 undef OWNER
 undef TABLE
+
