481d4a01e5c9a6cbc0bd8b89cfe93ea659ea861f
dhoogfr <freek.dhooge@gmail.com>
2016-08-22 19:13:27 +0200

    Rewrite list.sql
    Rewrite list.sql to better match the behaviour of how sqlplus matches script calls
    First it searches for files in the current directory, followed by the directories
    specified in the SQLPATH variable (in order of appearance).
    
    An optional second parameter will control the search depth and defaults to 1
    (no subdirectories are searched)
    
    The listing now contains the directory being searched, followed by the files found
    per directory

--- list.sql
+++ list.sql
@@ -1,1 +1,18 @@
-!for i in `echo $SQLPATH | tr ':' ' '`; do find $i -iname "*&1*.sql" -exec basename {} \; ; done
+-- list all files which names containing the passed string and ending in .sql, found in the current directory
+-- or in the directories found in the $SQLPATH environment variable
+-- the optional second parameter determines how many subdirectories will be searched and defaults to 1 (only the directories itself)
+
+set termout off
+set verify off
+
+column 2 new_value 2
+
+select '' "2" from dual where rownum = 0;
+define ldepth = '&2'
+
+set termout on
+
+
+!depth=&ldepth; for i in `echo ./ $SQLPATH | tr ':' ' '`; do echo ; echo $i; echo ; cd $i ; find ./ -maxdepth "${depth:-1}" -iname "*&1*.sql"  2> /dev/null; done
+
+undefine 2
