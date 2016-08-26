99fa4481189bf22a9650163232ba5686a39f7448
dhoogfr <freek.dhooge@gmail.com>
2016-08-26 13:01:04 +0200

    Add parameter cleanup and documentation

--- list.sql
+++ list.sql
@@ -5,6 +5,7 @@
 set termout off
 set verify off
 
+-- check if a second parameter has been passed to define te depth of the search
 column 2 new_value 2
 
 select '' "2" from dual where rownum = 0;
@@ -13,6 +14,10 @@ define ldepth = '&2'
 set termout on
 
 
+-- actual search, if no depth has been given, the depth is set to 1
 !depth=&ldepth; for i in `echo ./ $SQLPATH | tr ':' ' '`; do echo ; echo $i; echo ; cd $i ; find ./ -maxdepth "${depth:-1}" -iname "*&1*.sql" -printf "   * %P\n" 2> /dev/null; done
 
+
+-- cleanup
+undefine 1
 undefine 2
