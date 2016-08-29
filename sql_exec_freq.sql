0a133da6b895e889e22139db4268f0fa05daaabb
dhoogfr <freek.dhooge@gmail.com>
2016-08-29 14:51:15 +0200

    Restructure sql_exec_freq.sql and add plan_hash_value
    Restructure sql_exec_freq.sql script to improve readability and add the plan_hash_value field.

--- sql_exec_freq.sql
+++ sql_exec_freq.sql
@@ -1,17 +1,26 @@
-set linesize 120
+set linesize 150
 set pages 999
-column avg_elapsed_sec format 99999D99
-column disk_reads format 999999999
-column buffer_gets format 999999999
+column avg_elapsed_sec format 99G999D99
+column disk_reads format 9G999G99G999
+column buffer_gets format 999G999G999
 
-select to_char(snap.begin_interval_time, 'DD/MM/YYYY HH24:MI:SS') btime, 
-       to_char(snap.end_interval_time, 'DD/MM/YYYY HH24:MI:SS') etime, snap.snap_id, 
-       snap.instance_number, sqlstat.executions_delta nbr_executions, 
-       (sqlstat.elapsed_time_delta / decode(sqlstat.executions_delta, 0, 1, executions_delta)/1000000) avg_elapsed_sec,
-       (sqlstat.disk_reads_delta / decode(sqlstat.executions_delta, 0, 1, executions_delta)) disk_reads, 
-       (sqlstat.buffer_gets_delta / decode(sqlstat.executions_delta, 0, 1, executions_delta)) buffer_gets
-from dba_hist_snapshot snap, dba_hist_sqlstat sqlstat
-where snap.snap_id = sqlstat.snap_id(+)
-      and snap.instance_number = sqlstat.instance_number(+)
-      and sql_id = '&sql_id'
-order by snap.snap_id, snap.instance_number;
+select
+  to_char(snap.begin_interval_time, 'DD/MM/YYYY HH24:MI:SS')                                          btime, 
+  to_char(snap.end_interval_time, 'DD/MM/YYYY HH24:MI:SS')                                            etime, 
+  snap.snap_id,
+  plan_hash_value,
+  snap.instance_number, sqlstat.executions_delta                                                      nbr_executions, 
+  (sqlstat.elapsed_time_delta / decode(sqlstat.executions_delta, 0, 1, executions_delta)/1000000)     avg_elapsed_sec,
+  (sqlstat.disk_reads_delta / decode(sqlstat.executions_delta, 0, 1, executions_delta))               disk_reads, 
+  (sqlstat.buffer_gets_delta / decode(sqlstat.executions_delta, 0, 1, executions_delta))              buffer_gets
+from
+  dba_hist_snapshot snap, 
+  dba_hist_sqlstat sqlstat
+where 
+  snap.snap_id = sqlstat.snap_id(+)
+  and snap.instance_number = sqlstat.instance_number(+)
+  and sql_id = '&sql_id'
+order by 
+  snap.snap_id, 
+  snap.instance_number
+;
