4f41b60f18f271e2e6e70fd43b714bb8eeeac0cb
dhoogfr <freek.dhooge@gmail.com>
2016-09-01 22:22:13 +0200

    Fix find_trace.sql so it works correctly with diag architecture
    Change the find_trace.sql script to work with the diag architecture
    rather than just the user_dest

--- find_trace.sql
+++ find_trace.sql
@@ -1,25 +1,9 @@
 column tracefile_name for a120
 
-select 
-  ( rtrim(k.value,'/') || '/' || d.instance_name || '_ora_' || p.spid || 
-    decode(p.value, '', '', '_' || p.value) || '.trc' 
-  ) tracefile_name
-from 
-  v$parameter       k, 
-  v$parameter       p, 
-  v$instance        d,
-  sys.v_$session    s, 
-  sys.v_$process    p,
-  ( select 
-      sid 
-    from
-      v$mystat 
-    where 
-      rownum = 1
-  )                 m
+select
+  value tracefile_name
+from
+  v$diag_info
 where
-  p.name = 'tracefile_identifier'
-  and k.name = 'user_dump_dest'
-  and s.paddr = p.addr
-  and s.sid = m.sid
+  name = 'Default Trace File'
 ;
